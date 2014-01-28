a63a23bbd15720def3cd0d53b60dea39471f2d2f
BrunoDF <bdfachine@gmail.com>
2014-01-28 11:54:19 -0200

    Add Grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    precision: 9
+                },
+                files: {
+                    'assets/css/avant-fw.css' : 'assets/css/sass/avant-fw.scss'
+                }
+            }
+        },
+        watch: {
+            files: ['assets/css/sass/*'],
+            tasks: ['sass']
+        }
+    });
+    
+    // Load SASS Grunt plugin - Carrega o plugin SASS do Grunt
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    
+    // Watch
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    // Default Task
+    grunt.registerTask('default', ['sass', 'watch']);
+};

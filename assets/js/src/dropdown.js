3e360b63b5efcff5295a906310f7b959c8f6c1d8
BrunoDF <bdfachine@gmail.com>
2014-02-05 10:35:53 -0200

    Dropdown closes when another one is opened

--- assets/js/src/dropdown.js
+++ assets/js/src/dropdown.js
@@ -1,23 +1,27 @@
 var Dropdown = {
     btn: document.getElementsByClassName('dropdown_btn'),
-    toggle: function(el) {
-        el.parentNode.classList.toggle('visible');
-    },
     onWindowResize: function() {
         for (var i=0;i < Dropdown.btn.length; i++) {
             Dropdown.btn[i].parentNode.classList.remove('visible');
         }
+    },
+    visible: undefined,
+    toggle: function(el) {
+        el.parentNode.classList.toggle('visible');
     }
 }
+
+var arr = Array.prototype.slice.call(Dropdown.btn);
 
 for (var i=0;i < Dropdown.btn.length; i++) {
     Dropdown.btn[i].onclick = function() {
         Dropdown.toggle(this);
+        Dropdown.visible = arr.indexOf(this);
         
-        /*for (var j=0;j < Dropdown.btn.length; j++) {
-            if (j !== i) {
+        for (var j=0;j < Dropdown.btn.length; j++) {
+            if (j !== Dropdown.visible) {
                 Dropdown.btn[j].parentNode.classList.remove('visible');
             }
-        }*/
+        }
     }
 }

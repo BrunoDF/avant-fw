ece441755837b2ac2c87a4e352ec530d4429e6e8
BrunoDF <bdfachine@gmail.com>
2014-02-12 15:55:25 -0200

    Remove tooltip slide animation and add onhover and onfocus options

--- assets/js/src/tooltip.js
+++ assets/js/src/tooltip.js
@@ -1,13 +1,12 @@
 var Tooltip = {
     build: function(el) {
         var tooltip = document.createElement('div');
+        var content = el.hasAttribute('data-tooltip-hover') ? el.getAttribute('data-tooltip-hover') : el.getAttribute('data-tooltip-focus');
         tooltip.classList.add('tooltip');
-        tooltip.innerHTML = '<i></i><p>'+ el.getAttribute('data-tooltip') +'</p>';
+        tooltip.innerHTML = '<i></i><p>'+ content +'</p>';
         document.body.appendChild(tooltip);
         
         Tooltip.position(el, tooltip);
-        
-        setTimeout(function() { tooltip.classList.add('visible') }, 50);
     },
     hide: function() {
         var tooltip = document.querySelectorAll('.tooltip');
@@ -24,27 +23,42 @@ var Tooltip = {
         }
     },
     onDocumentReady: function() {
-        for (var i=0; i < Tooltip.trigger.length; i++) {
-            Tooltip.trigger[i].onmouseover = function() {
+        for (var i=0; i < Tooltip.trigger.onHover.length; i++) {
+            Tooltip.trigger.onHover[i].onmouseover = function() {
                 Tooltip.show(this);
             }
             
-            Tooltip.trigger[i].onmouseout = function() {
+            Tooltip.trigger.onHover[i].onmouseout = function() {
+                Tooltip.hide();
+            }
+        }
+        
+        for (var i=0; i < Tooltip.trigger.onFocus.length; i++) {
+            Tooltip.trigger.onFocus[i].onfocus = function() {
+                Tooltip.show(this);
+            }
+            
+            Tooltip.trigger.onFocus[i].onblur = function() {
                 Tooltip.hide();
             }
         }
     },
     position: function(el, tooltip) {
+        var tooltipDistance = (el.offsetHeight - (tooltip.firstChild.offsetHeight - 4));
         var posTop = el.offsetTop;
         var posLeft = el.offsetLeft;
         var center = (el.offsetWidth - tooltip.offsetWidth) / 2;
         
-        tooltip.style.top = posTop;
+        tooltip.style.top = posTop + tooltipDistance;
         tooltip.style.left = posLeft;
         tooltip.style.marginLeft = center;
+        
+        setTimeout(function() { 
+            tooltip.classList.add('visible');
+        }, 50);
     },
     show: function(el) {
         Tooltip.build(el);
     },
-    trigger: document.querySelectorAll('[data-tooltip]')
+    trigger: { onHover: document.querySelectorAll('[data-tooltip-hover]'), onFocus: document.querySelectorAll('[data-tooltip-focus]') }
 }
